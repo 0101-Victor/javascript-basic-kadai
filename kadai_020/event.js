5a43eb7b489d9ef426383f65ea7b1bcb7fabdec3
0101-Victor <rouge.of@gmail.com>
2025-07-04 10:03:48 +0900

    修正

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,8 +1,8 @@
-// ボタン要素とテキスト要素を取得
+// ボタン要素とテキスト要素を取得する
 const button = document.getElementById('btn');
 const textElement = document.getElementById('text');
 
-// ボタンにクリックイベントを設定
+// ボタンにクリックイベントを設定する
 button.addEventListener('click', function () {
   textElement.textContent = 'ボタンをクリックしました';
 });
